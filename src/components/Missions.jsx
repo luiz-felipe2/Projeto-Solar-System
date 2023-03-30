b82c3d16088b7ed5f395f7f99a08d5ea5146009e
Luiz Felipe <luiz.felipe213@gmail.com>
2023-03-30 17:36:28 -0300

    requisito 10 ok

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 export default class Missions extends Component {
   render() {
@@ -8,6 +10,15 @@ export default class Missions extends Component {
         <div data-testid="missions">Missions</div>
         <div>
           <Title headline="Missões" />
+          {missions.map(({ name, year, country, destination }) => {
+            const missoes = (<MissionCard
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />);
+            return missoes;
+          })}
         </div>
 
       </>
