3bb8340ea24ade93f444624877676f2ab347d9b0
Luiz Felipe <luiz.felipe213@gmail.com>
2023-03-30 17:12:18 -0300

    requisito 8 ok

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">Missions</div>
+      <>
+        <div data-testid="missions">Missions</div>
+        <div>
+          <Title headline="Missões" />
+        </div>
+
+      </>
     );
   }
 }
