e4515f601465a5afcc27041e532c32d16168c830
Luiz Felipe <luiz.felipe213@gmail.com>
2023-03-30 17:02:30 -0300

    requisito 6 ok

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planetas) => {
+          const { name, image } = planetas;
+          const planeta = <PlanetCard planetName={ name } planetImage={ image } />;
+          return planeta;
+        })}
       </div>
     );
   }
